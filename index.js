a94ee858f8f55306ad7e0f18de91b8db299d394e
Flavio Atas Medeiros <fatasmedeiros@deloitte.nl>
2020-06-30 17:10:56 +0200

    Working

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import range from "lodash/range";
 import dayjs from "dayjs";
 import weekday from "dayjs/plugin/weekday";
+import utc from "dayjs/plugin/utc";
 
 import "./index.css";
 dayjs.extend(weekday);
+dayjs.extend(utc);
 
 const [el] = document.getElementsByClassName("ptd");
 
-function renderDays(month) {
-  const lastDay = dayjs().set("month", month);
-  const weekDay = lastDay.weekday();
-  console.log(lastDay, weekDay);
-  console.log(weekDay - 1);
-  console.log(range(weekDay - 1));
-  const days = range(weekDay - 1)
+function renderDays(year, month) {
+  const lastDay = new Date(year, month + 1, 0);
+
+  const days = range(lastDay.getDay() - 1)
     .map((i) => "x")
-    .concat(range(1, parseInt(lastDay.format("D")) + 1));
+    .concat(range(1, lastDay.getDate() + 1));
+
   console.log(days);
 
   el.innerHTML = lastDay;
@@ -23,4 +23,15 @@ function renderDays(month) {
 
 // console.log(dayjs().set("month", 2));
 
-renderDays(1);
+// renderDays(2020, 0);
+renderDays(2020, 1);
+// renderDays(2020, 2);
+// renderDays(2020, 3);
+// renderDays(2020, 4);
+// renderDays(2020, 5);
+// renderDays(2020, 6);
+// renderDays(2020, 7);
+// renderDays(2020, 8);
+// renderDays(2020, 9);
+// renderDays(2020, 10);
+// renderDays(2020, 11);
