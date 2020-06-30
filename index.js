55d3e7795941d6b3c544a1dd0038cac5925daa03
Flavio Atas Medeiros <fatasmedeiros@deloitte.nl>
2020-06-30 16:34:14 +0200

    Printing some stuff

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+import range from "lodash/range";
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+
 import "./index.css";
+dayjs.extend(weekday);
 
 const [el] = document.getElementsByClassName("ptd");
 
 function renderDays(month) {
-  el.innerHTML = month;
+  const lastDay = dayjs().set("month", month);
+  const weekDay = lastDay.weekday();
+  console.log(lastDay, weekDay);
+  console.log(weekDay - 1);
+  console.log(range(weekDay - 1));
+  const days = range(weekDay - 1)
+    .map((i) => "x")
+    .concat(range(1, parseInt(lastDay.format("D")) + 1));
+  console.log(days);
+
+  el.innerHTML = lastDay;
 }
 
-renderDays(5);
+// console.log(dayjs().set("month", 2));
+
+renderDays(1);
